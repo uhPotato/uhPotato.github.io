456d9f573ee5cdcfca4cc5c528ef287b901dd34e
David Davies (V) <d_davies@apple.com>
2025-02-06 11:30:53 -0800

    Update for Small viewport Nav Menu

--- js/toggleNav.js
+++ js/toggleNav.js
@@ -1,6 +1,5 @@
 function toggleMenu() {
     const navbarMenu = document.getElementById("navbar-menu");
     navbarMenu.classList.toggle("show");
-    console.log(...[navbarMenu, '👀 [toggleNav.js:4]: navbarMenu'].reverse());
   }
   
